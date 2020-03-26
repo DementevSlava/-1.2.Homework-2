919c61975e8961d97bd5c06ec166b32997768e2a
Dementev Slava <kursant344@gmail.com>
2020-03-26 11:03:39 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Blok_1_VvedenieJava.Peremennye_operatory.Z2_Precision;
+
+public class Main {
+    public static void main(String[] args) {
+        double regularBonus = 0.3;
+        double specialBonus = 0.6;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
